3613ade9965d94fc4d7f07f585bfe25208c1161c
sairam <sairm21@gmail.com>
2023-09-09 15:45:20 +0530

    roboshop.conf parameters and values

--- main.tf
+++ main.tf
@@ -9,27 +9,27 @@ variable "parameters" {
   default = [
     {
       name = "roboshop.dev.frontend.catalogue_url",
-      value = "http://catalogue-dev.iamadevopsengineer.tech",
+      value = "http://catalogue-dev.iamadevopsengineer.tech:8080/",
       type = "String"
     },
     {
       name = "roboshop.dev.frontend.user_url",
-      value = "http://user-dev.iamadevopsengineer.tech",
+      value = "http://user-dev.iamadevopsengineer.tech:8080/",
       type = "String"
     },
     {
       name = "roboshop.dev.frontend.cart_url",
-      value = "http://cart-dev.iamadevopsengineer.tech",
+      value = "http://cart-dev.iamadevopsengineer.tech:8080/",
       type = "String"
     },
     {
       name = "roboshop.dev.frontend.shipping_url",
-      value = "http://shipping-dev.iamadevopsengineer.tech",
+      value = "http://shipping-dev.iamadevopsengineer.tech:8080/",
       type = "String"
     },
     {
       name = "roboshop.dev.frontend.payment_url",
-      value = "http://payment-dev.iamadevopsengineer.tech",
+      value = "http://payment-dev.iamadevopsengineer.tech:8080/",
       type = "String"
     }
   ]
