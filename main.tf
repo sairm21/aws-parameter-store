29710473a0f329d7d807157503f37bff26443f68
sairam <sairm21@gmail.com>
2023-09-09 14:55:21 +0530

    parameters demo with remote state file

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_ssm_parameter" "roboshop_params" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
+  value = var.parameters[count.index].value
+}
+
+variable "parameters" {
+  default = [
+    {
+      name = "demo1"
+      value = "demo1"
+      type = "string"
+    }
+    {
+      name = "demo2"
+      value = "demo2"
+      type = "string"
+    }
+  ]
+}
