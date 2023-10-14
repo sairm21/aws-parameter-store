406e2af9b97aace1e975b3a7bc197a845fbe821e
sairam <sairm21@gmail.com>
2023-10-14 17:01:09 +0530

    adding docdb credentials

--- main.tf
+++ main.tf
@@ -137,6 +137,16 @@ variable "parameters" {
     name = "roboshop.dev.mysql.password",
     value = "roboshop123",
     type = "SecureString"
+    },
+    {
+      name = "roboshop.dev.docdb.username",
+      value = "roboshop",
+      type = "String"
+    },
+    { #passwords will be never part of git repos they will be creted manually through console
+      name = "roboshop.dev.docdb.password",
+      value = "roboshop123",
+      type = "SecureString"
     }
   ]
 }
