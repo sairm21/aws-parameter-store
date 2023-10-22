a1442ab6cb2ebe0c54e680de0bc15815e8251691
sairam <sairm21@gmail.com>
2023-10-22 17:11:54 +0530

    adding https and removing port for nginx confif file parameters

--- main.tf
+++ main.tf
@@ -10,27 +10,27 @@ variable "parameters" {
   default = [
     {
       name = "roboshop.dev.frontend.catalogue_url",
-      value = "http://catalogue-dev.iamadevopsengineer.tech:8080/",
+      value = "https://catalogue-dev.iamadevopsengineer.tech/",
       type = "String"
     },
     {
       name = "roboshop.dev.frontend.user_url",
-      value = "http://user-dev.iamadevopsengineer.tech:8080/",
+      value = "https://user-dev.iamadevopsengineer.tech/",
       type = "String"
     },
     {
       name = "roboshop.dev.frontend.cart_url",
-      value = "http://cart-dev.iamadevopsengineer.tech:8080/",
+      value = "https://cart-dev.iamadevopsengineer.tech/",
       type = "String"
     },
     {
       name = "roboshop.dev.frontend.shipping_url",
-      value = "http://shipping-dev.iamadevopsengineer.tech:8080/",
+      value = "https://shipping-dev.iamadevopsengineer.tech/",
       type = "String"
     },
     {
       name = "roboshop.dev.frontend.payment_url",
-      value = "http://payment-dev.iamadevopsengineer.tech:8080/",
+      value = "https://payment-dev.iamadevopsengineer.tech/",
       type = "String"
     },
     {
